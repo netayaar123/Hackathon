0b289d471327871d3776465bdbc7b43d16f414ed
danielush <danielacohen@campus.technion.ac.il>
2025-01-12 15:33:04 +0200

    git add .
    "generate response" method that takes prompt and returns answer. to be used in promptBuilder

--- server/LLM/main.mjs
+++ server/LLM/main.mjs
@@ -1,11 +1,18 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-//import { generateTypePrompt, generateFactVerificationPrompt, generateOpinionOrLiePrompt } from "../controllers/promptBuilder.js";
 
 const genAI = new GoogleGenerativeAI("AIzaSyA4KvH0d4rPdu7fdqziqJX6_-bC7omAJnk");
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "is the sky green?";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+async function generateResponse(prompt) {
+    try {
+      const result = await model.generateContent(prompt);
+      console.log(result.response.text());
+      return result.response.text();       
+    } 
+    catch (error) {
+      console.error("Error generating content:", error); 
+      throw error;
+    }
+  }
+  //use this in prompt to generate response 
