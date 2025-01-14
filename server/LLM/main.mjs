a5ccbebf4928732f27ff87566a6ca653ec678705
danielush <danielacohen@campus.technion.ac.il>
2025-01-14 10:44:06 +0200

    moved token to .env file

--- server/LLM/main.mjs
+++ server/LLM/main.mjs
@@ -1,18 +1,30 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
 
-const genAI = new GoogleGenerativeAI("AIzaSyA4KvH0d4rPdu7fdqziqJX6_-bC7omAJnk");
+// Load the `token.env` file
+dotenv.config({ path: "./server/token.env" });  
+
+// Access the API key from the .ev file
+const apiKey = process.env.GEMINI_API_KEY;
+
+if (!apiKey) {
+  throw new Error("API key is missing! Please add GEMINI_API_KEY to token.env.");
+}
+
+const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function generateResponse(prompt) {
   try {
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-    return result.response.text();
-  }
+    console.log(result.response.text());  
+    return result.response.text();  
+  } 
   catch (error) {
     console.error("Error generating content:", error);
-    throw error;
+    throw error; 
   }
 }
+
 export { generateResponse };
